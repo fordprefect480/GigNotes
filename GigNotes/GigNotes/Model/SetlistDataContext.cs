b7c74be271776ae5f994d968d4bb9722af182a50
Owen <owen.symes@gmail.com>
2013-04-10 18:24:08 +1200

    Proper actual solution commit

---
+++ GigNotes/GigNotes/Model/SetlistDataContext.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+namespace GigNotes.Model
+{
+    public class SetlistDataContext : DataContext
+    {
+        // Pass the connection string to the base class.
+        public SetlistDataContext(string connectionString)
+            : base(connectionString)
+        { }
+
+        public Table<Setlist> Setlists;
+    }
+
+    [Table]
+    public class Setlist : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        // Version column aids update performance.
+        [Column(IsVersion = true)]
+        private Binary _version;
+
+        // Define ID: private field, public property, and database column.
+        private int _setlistId;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int SetlistId
+        {
+            get { return _setlistId; }
+            set
+            {
+                if (_setlistId != value)
+                {
+                    NotifyPropertyChanging("SetlistId");
+                    _setlistId = value;
+                    NotifyPropertyChanged("SetlistId");
+                }
+            }
+        }
+
+        private string _name;
+
+        [Column]
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    NotifyPropertyChanging("Name");
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private DateTime? _gigDate;
+        
+        [Column(CanBeNull = true)]
+        public DateTime? GigDate
+        {
+            get { return _gigDate; }
+            set
+            {
+                if (_gigDate != value)
+                {
+                    NotifyPropertyChanging("GigDate");
+                    _gigDate = value;
+                    NotifyPropertyChanged("GigDate");
+                }
+            }
+        }
+
+        public string GigDateFormatted
+        {
+            get { return GigDate.Value.ToString("dd/MM/yyyy"); }
+        }
+
+        private string _additionalInfo;
+
+        [Column(CanBeNull = true)]
+        public string AdditionalInfo
+        {
+            get { return _additionalInfo; }
+            set
+            {
+                if (_additionalInfo != value)
+                {
+                    NotifyPropertyChanging("AdditionalInfo");
+                    _additionalInfo = value;
+                    NotifyPropertyChanged("AdditionalInfo");
+                }
+            }
+        }
+
+        //private List<Set> _sets;
+
+        //public List<Set> Sets
+        //{
+        //    get 
+        //    {
+        //        if (_sets == null)
+        //            _sets = new List<Set>();
+
+        //        return _sets; 
+        //    }
+        //    private set {}
+        //}
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify that a property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify that a property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+
+
+}
---
+++ GigNotes/GigNotes/ViewModels/SetlistManagerViewModel.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GigNotes.Resources;
+using GigNotes.Model;
+
+namespace GigNotes.ViewModels
+{
+    public class SetlistManagerViewModel : INotifyPropertyChanged
+    {
+        private SetlistDataContext setlistDB;
+
+        public SetlistManagerViewModel(string setlistDBConnectionString)
+        {
+            setlistDB = new SetlistDataContext(setlistDBConnectionString);
+        }
+
+        private ObservableCollection<Setlist> _setlists;
+        public ObservableCollection<Setlist> Setlists
+        {
+            get { return _setlists; }
+            set
+            {
+                _setlists = value;
+                NotifyPropertyChanged("Setlists");
+            }
+        }
+        
+        public void LoadCollectionsFromDatabase()
+        {
+            // Query the database and load all to-do items.
+            Setlists = new ObservableCollection<Setlist>(setlistDB.Setlists);          
+        }
+
+        // Add a to-do item to the database and collections.
+        public void AddSetlist(Setlist newSetlist)
+        {
+            // Add a to-do item to the data context.
+            setlistDB.Setlists.InsertOnSubmit(newSetlist);
+
+            // Save changes to the database.
+            setlistDB.SubmitChanges();
+
+            // Add a to-do item to the "all" observable collection.
+            Setlists.Add(newSetlist);
+        }
+
+        // Remove a to-do task item from the database and collections.
+        public void DeleteSetlist(Setlist setlistForDelete)
+        {
+
+            // Remove the to-do item from the "all" observable collection.
+            Setlists.Remove(setlistForDelete);
+
+            // Remove the to-do item from the data context.
+            setlistDB.Setlists.DeleteOnSubmit(setlistForDelete);
+
+            // Save changes to the database.
+            setlistDB.SubmitChanges();
+        }
+
+        // Write changes in the data context to the database.
+        public void SaveChangesToDB()
+        {
+            setlistDB.SubmitChanges();
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify Silverlight that a property has changed.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        ///// <summary>
+        ///// A collection for SetlistViewModel objects.
+        ///// </summary>
+        //public ObservableCollection<SetlistViewModel> Setlists { get; private set; }
+
+        //private string _sampleProperty = "Sample Runtime Property Value";
+        ///// <summary>
+        ///// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        ///// </summary>
+        ///// <returns></returns>
+        //public string SampleProperty
+        //{
+        //    get
+        //    {
+        //        return _sampleProperty;
+        //    }
+        //    set
+        //    {
+        //        if (value != _sampleProperty)
+        //        {
+        //            _sampleProperty = value;
+        //            NotifyPropertyChanged("SampleProperty");
+        //        }
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Sample property that returns a localized string
+        ///// </summary>
+        //public string LocalizedSampleProperty
+        //{
+        //    get
+        //    {
+        //        return AppResources.SampleProperty;
+        //    }
+        //}
+
+        //public bool IsDataLoaded
+        //{
+        //    get;
+        //    private set;
+        //}
+
+        ///// <summary>
+        ///// Creates and adds a few ItemViewModel objects into the Items collection.
+        ///// </summary>
+        //public void LoadData()
+        //{
+            // Sample data; replace with real data
+            //this.Items.Add(new ItemViewModel() { ID = "0", LineOne = "runtime one", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            //this.Items.Add(new ItemViewModel() { ID = "1", LineOne = "runtime two", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            //this.Items.Add(new ItemViewModel() { ID = "2", LineOne = "runtime three", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            //this.Items.Add(new ItemViewModel() { ID = "3", LineOne = "runtime four", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            //this.Items.Add(new ItemViewModel() { ID = "4", LineOne = "runtime five", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            //this.Items.Add(new ItemViewModel() { ID = "5", LineOne = "runtime six", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            //this.Items.Add(new ItemViewModel() { ID = "6", LineOne = "runtime seven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            //this.Items.Add(new ItemViewModel() { ID = "7", LineOne = "runtime eight", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+            //this.Items.Add(new ItemViewModel() { ID = "8", LineOne = "runtime nine", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu" });
+            //this.Items.Add(new ItemViewModel() { ID = "9", LineOne = "runtime ten", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus" });
+            //this.Items.Add(new ItemViewModel() { ID = "10", LineOne = "runtime eleven", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Habitant inceptos interdum lobortis nascetur pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent" });
+            //this.Items.Add(new ItemViewModel() { ID = "11", LineOne = "runtime twelve", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Ultrices vehicula volutpat maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos" });
+            //this.Items.Add(new ItemViewModel() { ID = "12", LineOne = "runtime thirteen", LineTwo = "Maecenas praesent accumsan bibendum", LineThree = "Maecenas praesent accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur" });
+            //this.Items.Add(new ItemViewModel() { ID = "13", LineOne = "runtime fourteen", LineTwo = "Dictumst eleifend facilisi faucibus", LineThree = "Pharetra placerat pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent" });
+            //this.Items.Add(new ItemViewModel() { ID = "14", LineOne = "runtime fifteen", LineTwo = "Habitant inceptos interdum lobortis", LineThree = "Accumsan bibendum dictumst eleifend facilisi faucibus habitant inceptos interdum lobortis nascetur pharetra placerat" });
+            //this.Items.Add(new ItemViewModel() { ID = "15", LineOne = "runtime sixteen", LineTwo = "Nascetur pharetra placerat pulvinar", LineThree = "Pulvinar sagittis senectus sociosqu suscipit torquent ultrices vehicula volutpat maecenas praesent accumsan bibendum" });
+
+        //    this.IsDataLoaded = true;
+        //}
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //private void NotifyPropertyChanged(String propertyName)
+        //{
+        //    PropertyChangedEventHandler handler = PropertyChanged;
+        //    if (null != handler)
+        //    {
+        //        handler(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+    }
+}
